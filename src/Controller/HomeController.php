0c86dda8732dee81b0d085b3fe596522dcd32804
IveliosNailo <tristan.dupontroue@gmail.com>
2025-06-17 11:00:31 +0200

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController {
+
+#[Route("/",name : "home")]
+
+ function index (Request $request): Response {
+    return new Response ('Bonjour ' .$request->query->get('name'));
+
+}
+
+}
---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class RecipeController extends AbstractController
+{
+   
+   #[Route('/recette', name: 'recipe.index',)]
+    public function index(HttpFoundationRequest $request): Response
+    {
+      return $this->render('recipe/index.html.twig');
+    }
+   
+    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id'=>'\d+', 'slug'=>'[a-z0-9-]+'])]
+    public function show(HttpFoundationRequest $request, string $slug, int $id): Response
+    {
+        return new Response('Recette : '. $slug);
+    }
+}
